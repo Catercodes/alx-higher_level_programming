8d1de617aaa37370e6e904fb5dc086995f7d6f9a
Catercodes <chrisadamsadewuni@gmail.com>
2023-12-27 09:10:14 +0100

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""The Shebang """
+
+
+class Square:
+    """The square class """
+
+    def __init__(self, size=0):
+        """private attribute"""
+        if not isinstance(size, int):
+            """raising Exceptions"""
+            raise TypeError("size must be an integer")
+        if size < 0:
+            """ raising ValueErro Exception"""
+            raise ValueError("size must be >= 0")
