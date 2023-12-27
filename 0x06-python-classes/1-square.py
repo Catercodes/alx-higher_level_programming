15f69564767d08c864b45fbe00a1a19b5420b667
Catercodes <chrisadamsadewuni@gmail.com>
2023-12-27 07:49:48 +0100

    A private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The Shebang command """
+
+
+class Square:
+    """This is the Square class"""
+
+    def __init__(self, size):
+        """private instance"""
+        self.__size = size
