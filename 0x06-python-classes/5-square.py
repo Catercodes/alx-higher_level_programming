7d8ebc66216b387d049e6bd01a00603a3abf51bc
Catercodes <chrisadamsadewuni@gmail.com>
2023-12-30 15:52:55 +0100

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""The Shebang """
+
+
+class Square:
+    """The square class """
+
+    def __init__(self, size=0):
+        """private attribute"""
+        self.__size = size
+        """ A TypeError"""
+        if not isinstance(size, int):
+            """raising Exceptions"""
+            raise TypeError("size must be an integer")
+        if size < 0:
+            """ raising ValueErro Exception"""
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """property 'def size(self):' to retrieve it"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        """property setter"""
+        if not isinstance(value, int):
+            """Exceptions"""
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """ calculates  the area of square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """ This function prints square"""
+        if self.__size < 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                print(self.__size * '#')
