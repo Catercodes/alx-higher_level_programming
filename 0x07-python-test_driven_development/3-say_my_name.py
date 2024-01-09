d1b7b2c30596f2d7983199effeb787b938df8d5a
Catercodes <chrisadamsadewuni@gmail.com>
2024-01-09 09:18:07 +0100

    say_my_name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -14,8 +14,10 @@ def say_my_name(first_name, last_name=""):
     """
 
     if not isinstance(first_name, str):
+        """ raising exception"""
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
+        """ raising exception"""
         raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
