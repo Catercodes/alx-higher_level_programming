3af0ff72fd4fd6896f06ec1ce0e5d96b038b82b2
Catercodes <chrisadamsadewuni@gmail.com>
2024-01-10 16:19:27 +0100

    Unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max_number(self):
+        """This check for max_num along the real numbers"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_negative_number(self):
+        """ This checks for max_num within the negative numbers"""
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_real_and_negative(self):
+        """ This checks the max_number betwwen positive and negative"""
+        self.assertEqual(max_integer([-4, 1]), 1)
+
+    def test_strings(self):
+        """This test strings """
+        with self.assertRaises(TypeError):
+            max_integer([1, "string", 5])
+
+    def test_float_number(self):
+        """Check for flaot_numberds"""
+        self.assertEqual(max_integer([3.4, 5.7]), 5.7)
+
+    def test_empty_list(self):
+        """ check empty list"""
+        self.assertIsNone(max_integer([]), "Empty list return None")
