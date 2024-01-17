a91a478d900735c4d543000322ea6d53863a747d
Catercodes <chrisadamsadewuni@gmail.com>
2024-01-17 12:56:08 +0100

    Instance of object and Int

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" The Python Command"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ This checks for instance of object , int"""
+    return isinstance(obj, a_class)
+    False
