c6fd0b4c32220f975024bb37cb97242a3ecda2dc
Catercodes <chrisadamsadewuni@gmail.com>
2024-01-18 21:22:07 +0100

    6-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" The command python"""
+
+
+class BaseGeometry:
+    def area(self):
+        """ raise a modified Exception"""
+        raise Exception("area() is not implemented")
