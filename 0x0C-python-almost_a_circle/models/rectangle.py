00ea0786785e8ed6f6c009812e726b8e47c697d7
Catercodes <chrisadamsadewuni@gmail.com>
2024-01-21 06:37:54 +0100

    Validator

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,6 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """ THe Rectangle class inherited from base"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Call the constructor of the base class """
         super().__init__(id)
@@ -23,6 +24,9 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """ The setter for width"""
+        self.integer_validator(value, "width")
+        if value <= 0:
+            raise ValueError("width must > 0")
         self.__width = value
 
     @property
@@ -33,6 +37,9 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """ The setter for  height"""
+        self.integer_validator(value, "height")
+        if value <= 0:
+            raise ValueError("height must > 0")
         self.__height = value
 
     @property
@@ -43,6 +50,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """The setter for x"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be > 0")
         self.__x = value
 
     @property
@@ -53,4 +64,13 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """The settert for Y"""
+
+        self.integer_validator(value, "y")
+        if value < 0:
+            raise ValueError("y must be > 0")
         self.__y = value
+
+    def integer_validator(self, value, name):
+        """ This validetor"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
