63b1fd237d37db29d2e4260f1376260129830b01
Catercodes <chrisadamsadewuni@gmail.com>
2024-02-06 14:23:12 +0100

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-#""" The Shebang """
-#def write_file(filename="", text=""):
-#   with open(filename, mode='w', encoding="utf-8") as f:
-#       data = f.write(text)
-#       return data
-#!/usr/bin/python3
 """Contains function read_file"""
 
 
