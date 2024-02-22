7b7efb08757c0c8fcc93b85bd3111c202e1f1959
Catercodes <chrisadamsadewuni@gmail.com>
2024-02-22 21:40:42 +0100

    Task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Select States  with name Module"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    query = "SELECT *\
+            FROM states\
+            WHERE states.name = %s\
+            ORDER BY states.id ASC;"
+
+    db = MySQLdb.connect(user=username, password=password, database=database)
+    cur = db.cursor()
+    cur.execute(query, (state_name,))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
