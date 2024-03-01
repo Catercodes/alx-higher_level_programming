5bdc01bb387ad64ecb5fde00384073f61831c550
Catercodes <chrisadamsadewuni@gmail.com>
2024-03-01 22:47:26 +0100

    Fecting Url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetches 'https://alx-intranet.hbtn.io/status'"""
+from urllib import request
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    with request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
