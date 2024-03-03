7688d5dba130b09b772aef85df1b2afd8b5c35d4
Catercodes <chrisadamsadewuni@gmail.com>
2024-03-03 23:38:59 +0100

    size of a url

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Takes in a URL, sends a request to that URL, and dGGisplays the size of the body of the response
-curl -sI "$1" | grep Content-Length
+# Takes in a URL, sends a request to the URL, and display the size of the body of the response
+curl -sI "$1" | grep Content-Length | cut -d ' ' -f 2
