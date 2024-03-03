cadc884685a8835aabe9830415444c0794ffcc5a
Catercodes <chrisadamsadewuni@gmail.com>
2024-03-03 23:34:29 +0100

    size of a url

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Takes in a URL, sends a request to that URL, and dGGisplays the size of the body of the response
-#curl -sI $1 |grep -i Content_Length|awk |'{print $1}'
 curl -sI "$1" | grep Content-Length
