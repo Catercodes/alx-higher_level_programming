9cf5b18ac1fe024828cae8edd84acf1667a39ba7
Catercodes <chrisadamsadewuni@gmail.com>
2024-03-18 22:22:43 +0100

    Locked_Class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,7 +2,7 @@
 """ The python package """
 
 
-class LockedClas:
+class LockedClass:
     """Locked class that allow no dynamically creation of instances
     attributes except named first_nam"""
     __slots__ = ['first_name']
